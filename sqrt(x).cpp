c217edd3bc901ebdc438a933315f06cbcb7d3faf
prod-chirag <chiragluitel32@gmail.com>
2025-04-07 15:09:03 +1000

    in progress

---
+++ sqrt(x).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int mySqrt(int x){
+    
+    return 0;
+};
+
+int main(){
+    int input;
+    input = 0;
+    int result = mySqrt(input);
+    cout<< result << endl;
+    return 0;
+}
