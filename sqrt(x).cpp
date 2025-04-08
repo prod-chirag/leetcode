eeb39ec72617690a6b8cf264861dbce0c57527f1
prod-chirag <chiragluitel32@gmail.com>
2025-04-08 17:30:14 +1000

    completed

--- sqrt(x).cpp
+++ sqrt(x).cpp
@@ -3,13 +3,28 @@
 using namespace std;
 
 int mySqrt(int x){
-    
-    return 0;
+    int start = 0;
+    int end = x;
+
+    while(start<=end){
+        int mid = (start + end + 1)>>1;
+
+        int midsum = mid * mid;
+
+        if (midsum == x){
+            return mid;
+        }else if(midsum > x){
+            end = mid - 1;
+        }else if(midsum<x){
+            start = mid + 1;
+        }
+    };
+    return -1;
 };
 
 int main(){
     int input;
-    input = 0;
+    input = 10;
     int result = mySqrt(input);
     cout<< result << endl;
     return 0;
