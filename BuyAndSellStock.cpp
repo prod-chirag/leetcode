dc26ad6b8a545cb8ae1ad0710b2f3282398d2f46
prod-chirag <chiragluitel32@gmail.com>
2025-05-19 22:11:03 +1000

    WIP

---
+++ BuyAndSellStock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices){
+    int max = 0;
+    //10,1,5,6,7,1
+    int l = 0;
+    int r = 1;
+    for ( int i=0; i<prices.size(); i++){
+        //i = 0;
+        if(prices[l] >= prices[r] ){
+            l++;
+            r++;
+        }else{
+            if (prices[r] - prices[l] > max){
+                max =  prices[r] - prices[l];
+                r++;
+            }else{
+                r ++;
+            }
+        }
+    }
+    return max;
+}
+
+int main(){
+    vector<int> prices = {10,1,5,6,1,7};
+    int result = maxProfit(prices);
+    cout << result << endl;
+    return 0;
+}
